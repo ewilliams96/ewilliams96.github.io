9b044eb06eab359b720bae509ad41a183523e2e5
Elizabeth Williams <elizabeth.williams.1@stonybrook.edu>
2016-12-23 00:05:32 -0500

    fixed up links and things, should b ready?

--- js/text.js
+++ js/text.js
@@ -1,5 +1,6 @@
-/* Referenced and used http://stackoverflow.com/questions/7264974/show-text-letter-by-letter*/
+$('#expand').css("color", "rgb(30,30,30)")
 
+/* Referenced and used http://stackoverflow.com/questions/7264974/show-text-letter-by-letter*/
 function type_text(text, target, index, interval){
     if(index < text.length){
         $(target).append(text[index]);
@@ -8,18 +9,32 @@ function type_text(text, target, index, interval){
             type_text(text, target, index, interval-10);
         }, interval);
     }
-
 }
 
 $(function () { 
-    console.log("call")
   type_text("Hi, I'm Elizabeth!", "#greeting", 0, 300);    
- 
 }); 
 
+// show and hide further about/info stuff at top on button click
+var expanded = false;
 function expand(){
-    console.log("d")
-    $('#about').css("opacity", 1);
-    $('#about').css("height", "auto");
-    $('#expand').css("display", "none");
+    if(!expanded){
+        $('#ex').removeClass("fa-plus");
+        $('#ex').addClass("fa-minus");
+        $('#about').css("opacity", 1);
+        $('#about').css("height", "auto");
+        expanded = true;
+    }
+    else{
+        hide();
+    }
+
+}
+
+function hide(){
+    expanded = false;
+    $('#ex').removeClass("fa-minus");
+    $('#ex').addClass("fa-plus");
+    $('#about').css("opacity", 0);
+    $('#about').css("height", 0);
 }
